d011f716e700fd9dd5691eed97832fe72dd0dc41
Rupesh Sonkamble <rupeshsonkamble30@gmail.com>
2022-11-15 23:23:27 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,43 @@
+import json
+f=open("quiz-data.json")
+data=json.load(f)
+print(data)
+
+
+
+
+total_ques=0
+correct_ans=0
+
+
+for ques in data:
+
+    print(ques.get("question"))
+
+    print("options")
+
+    choices=ques.get("choices")
+
+    for i,choice in enumerate(choices):
+
+        print(i,":",choice)
+
+    user_choice=int(input("enter your choice : "))
+    total_ques+=1
+    if user_choice>4 or user_choice<0:
+        print("Invalid")
+    else:
+        if choices[user_choice]==ques.get("correct_ans"):
+            print("Your answer is correct : ",ques.get("correct_ans"))
+            correct_ans+=1
+        else:
+            print("Your answer is wrong")
+            print("The correct answer is : ",ques.get("correct_ans"))
+
+    
+
+
+    print()
+
+print("Total Attempted ques:",total_ques)
+print("Total correct answers:",correct_ans)
